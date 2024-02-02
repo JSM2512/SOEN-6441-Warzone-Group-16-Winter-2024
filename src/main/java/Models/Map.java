04f2af1d0206c4ae8af74c08f4211411a832c564
PV-68 <poonamvekariyapv@gmail.com>
2024-02-02 15:59:46 -0500

    Model added - Map

---
+++ src/main/java/Models/Map.java
@@ -0,0 +1,54 @@
+package Models;
+
+import java.sql.SQLOutput;
+import java.util.List;
+public class Map {
+
+    String d_mapName;
+    List<Country> d_mapCountries;
+    List<Continent> d_mapContinents;
+
+    public String getD_mapName() {
+        return d_mapName;
+    }
+
+    public void setD_mapName(String d_mapName) {
+        this.d_mapName = d_mapName;
+    }
+
+    public List<Country> getD_mapCountries() {
+        return d_mapCountries;
+    }
+
+    public void setD_mapCountries(List<Country> d_mapCountries) {
+        this.d_mapCountries = d_mapCountries;
+    }
+
+    public List<Continent> getD_mapContinents() {
+        return d_mapContinents;
+    }
+
+    public void setD_mapContinents(List<Continent> d_mapContinents) {
+        this.d_mapContinents = d_mapContinents;
+    }
+    public Country getCountryByName(String p_countryName){
+
+        for(Country l_country : d_mapCountries){
+            if(l_country.d_countryName.equals(p_countryName)){
+                return l_country;
+            }
+        }
+        System.out.println("The entered country is not present.");
+        return null;
+    }
+    public Continent getContinentByName(String p_continentName){
+
+        for(Continent l_continent : d_mapContinents){
+            if(l_continent.d_continentName.equals(p_continentName)){
+                return l_continent;
+            }
+        }
+        System.out.println("The entered continent is not present.");
+        return null;
+    }
+}
