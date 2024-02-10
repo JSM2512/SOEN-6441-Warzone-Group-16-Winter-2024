84f3e1f0cd1973e3ce1c5d8f9c92cc233141fe4a
pankaj0412 <spankajk.53@gmail.com>
2024-02-10 05:58:28 +0530

    Game player add function

--- src/main/java/Models/CurrentState.java
+++ src/main/java/Models/CurrentState.java
@@ -1,5 +1,6 @@
 package Models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CurrentState {
@@ -31,7 +32,32 @@ public class CurrentState {
         this.d_map = d_map;
     }
 
-    public void addOrRemoveGamePlayers(String operation, String arguments) {
+    public void addOrRemoveGamePlayers(String p_operation, String p_arguments) {
         System.out.println("current state player class");
+        if(p_operation.equals("add")){
+            addGamePlayer(p_arguments);
+        }
+    }
+    public void addGamePlayer(String p_arguments){
+        if (p_arguments.split(" ").length == 1) {
+            String l_playerName = p_arguments.split(" ")[0];
+            Player l_gamePlayer = new Player(l_playerName);
+            if(d_players == null){
+                d_players = new ArrayList<>();
+                d_players.add(l_gamePlayer);
+            }
+            else {
+                for(Player l_eachPlayer : d_players){
+                    if(l_eachPlayer.getD_name().equals(l_playerName))
+                    {
+                        System.out.println("Name already exist, try some other name!");
+                        return;
+                    }
+                }
+                d_players.add(l_gamePlayer);
+
+            }
+            System.out.println(l_playerName+" added.");
+        }
     }
 }
