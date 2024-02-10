cd9f196538d71a51bc0e80e5afab257259be8e3e
pankaj0412 <spankajk.53@gmail.com>
2024-02-10 06:32:16 +0530

    Added gameplayer remove function

--- src/main/java/Models/CurrentState.java
+++ src/main/java/Models/CurrentState.java
@@ -33,11 +33,50 @@ public class CurrentState {
     }
 
     public void addOrRemoveGamePlayers(String p_operation, String p_arguments) {
-        System.out.println("current state player class");
         if(p_operation.equals("add")){
             addGamePlayer(p_arguments);
         }
+        else if (p_operation.equals("remove")) {
+            removeGamePlayer(p_arguments);
+        }
+        else {
+            System.out.println("Wrong command entered. Use either '-add playerName' or '-remove playerName' in command.");
+        }
     }
+
+    private void removeGamePlayer(String p_arguments) {
+        if (p_arguments.split(" ").length == 1) {
+            String l_playerName = p_arguments.split(" ")[0];
+            if(d_players == null || d_players.isEmpty()){
+                System.out.println("No Player exist in the game yet.");
+            }
+            else {
+                boolean l_isPresent = false;
+                for(Player l_eachPlayer : d_players){
+                    if(l_eachPlayer.getD_name().equals(l_playerName)){
+                        l_isPresent = true;
+                    }
+                }
+                if(l_isPresent){
+                    d_players.remove(getPlayerFromName(l_playerName));
+                    System.out.println("Player : "+l_playerName+" removed from the game.");
+                }
+                else {
+                    System.out.println("Player : "+l_playerName+" does not exist.");
+                }
+            }
+        }
+    }
+
+    private Player getPlayerFromName(String p_playerName) {
+        for(Player l_eachPlayer : d_players){
+            if(l_eachPlayer.getD_name().equals(p_playerName)){
+                return l_eachPlayer;
+            }
+        }
+        return null;
+    }
+
     public void addGamePlayer(String p_arguments){
         if (p_arguments.split(" ").length == 1) {
             String l_playerName = p_arguments.split(" ")[0];
