6ce7afe0829ec966050d929ef12c2ccb74901e31
amirchappalwala <chappalwalaamir@gmail.com>
2024-02-13 19:23:28 -0800

    FIX : Validating if country belongs to the player

--- src/main/java/Controller/PlayerController.java
+++ src/main/java/Controller/PlayerController.java
@@ -14,17 +14,16 @@ public class PlayerController {
         int l_noOfPlayers = l_players.size();
         int l_noOfCountries = l_countryList.size();
         int l_noOfCountiesToEachPlayer = Math.floorDiv(l_noOfCountries, l_noOfPlayers);
-        assignRandomCountriesToPlayers(l_players,l_countryList,l_noOfCountiesToEachPlayer);
+        assignRandomCountriesToPlayers(l_players, l_countryList, l_noOfCountiesToEachPlayer);
         displayAssignedCountries(l_players);
-        assignContinentToPlayers(l_players,p_currentState.getD_map().getD_mapContinents());
+        assignContinentToPlayers(l_players, p_currentState.getD_map().getD_mapContinents());
     }
 
     private void displayAssignedCountries(List<Player> p_players) {
-        for(Player l_currentPlayer : p_players)
-        {
-            System.out.print("Player "+l_currentPlayer.getD_name()+" has these countries: ");
-            for(Country l_currentCountry : l_currentPlayer.getD_currentCountries()){
-                System.out.print(l_currentCountry.getD_countryName()+ " ");
+        for (Player l_currentPlayer : p_players) {
+            System.out.print("Player " + l_currentPlayer.getD_name() + " has these countries: ");
+            for (Country l_currentCountry : l_currentPlayer.getD_currentCountries()) {
+                System.out.print(l_currentCountry.getD_countryName() + " ");
             }
             System.out.println();
         }
@@ -32,13 +31,13 @@ public class PlayerController {
 
 
     private void assignContinentToPlayers(List<Player> p_players, List<Continent> p_mapContinents) {
-        for (Player l_eachPlayer : p_players){
+        for (Player l_eachPlayer : p_players) {
             List<Country> l_countriesOwnedByPlayer = l_eachPlayer.getD_currentCountries();
 
-            if (l_eachPlayer.getD_currentCountries() != null && !l_eachPlayer.getD_currentCountries().isEmpty()){
-                for(Continent l_eachContinent : p_mapContinents){
+            if (l_eachPlayer.getD_currentCountries() != null && !l_eachPlayer.getD_currentCountries().isEmpty()) {
+                for (Continent l_eachContinent : p_mapContinents) {
                     boolean l_isContinentOwned = l_countriesOwnedByPlayer.containsAll(l_eachContinent.getD_countries());
-                    if(l_isContinentOwned){
+                    if (l_isContinentOwned) {
                         l_eachPlayer.setContinent(l_eachContinent);
                     }
                 }
@@ -48,19 +47,19 @@ public class PlayerController {
 
     private void assignRandomCountriesToPlayers(List<Player> p_players, List<Country> p_countryList, int p_noOfCountiesToEachPlayer) {
         List<Country> l_unallocatedCountries = new ArrayList<>(p_countryList);
-        if (l_unallocatedCountries.isEmpty()){
+        if (l_unallocatedCountries.isEmpty()) {
             System.out.println("No Countries in Map.");
             return;
         }
-        for(Player l_eachPlayer : p_players){
-            for (int i = 1 ; i <= p_noOfCountiesToEachPlayer ; i++){
-                if(l_unallocatedCountries.isEmpty()){
+        for (Player l_eachPlayer : p_players) {
+            for (int i = 1; i <= p_noOfCountiesToEachPlayer; i++) {
+                if (l_unallocatedCountries.isEmpty()) {
                     break;
                 }
                 Random l_randomNumber = new Random();
                 int l_randomIndex = l_randomNumber.nextInt(l_unallocatedCountries.size());
 
-                if (l_eachPlayer.getD_currentCountries() == null){
+                if (l_eachPlayer.getD_currentCountries() == null) {
                     l_eachPlayer.setD_currentCountries(new ArrayList<>());
                 }
                 l_eachPlayer.getD_currentCountries().add(l_unallocatedCountries.get(l_randomIndex));
@@ -69,26 +68,26 @@ public class PlayerController {
         }
 
         if (!l_unallocatedCountries.isEmpty()) {
-            assignRandomCountriesToPlayers(p_players, l_unallocatedCountries,1);
+            assignRandomCountriesToPlayers(p_players, l_unallocatedCountries, 1);
         }
     }
 
     public void assignArmies(CurrentState p_currentState) {
-        for(Player l_eachPlayer : p_currentState.getD_players()){
+        for (Player l_eachPlayer : p_currentState.getD_players()) {
             int l_countOfArmiesOfEachPlayer = getNoOfArmies(l_eachPlayer);
             l_eachPlayer.setD_unallocatedArmies(l_countOfArmiesOfEachPlayer);
-            System.out.println("Player : "+l_eachPlayer.getD_name()+" got assigned : "+ l_countOfArmiesOfEachPlayer + " Armies.");
+            System.out.println("Player " + l_eachPlayer.getD_name() + " got assigned : " + l_countOfArmiesOfEachPlayer + " Armies.");
         }
     }
 
     private int getNoOfArmies(Player p_eachPlayer) {
         int l_currentArmySize = 0;
-        if(p_eachPlayer.getD_currentCountries() != null && !p_eachPlayer.getD_currentCountries().isEmpty()){
-            l_currentArmySize = Math.max(3, Math.round((float)(p_eachPlayer.getD_currentCountries().size() / 3)));
+        if (p_eachPlayer.getD_currentCountries() != null && !p_eachPlayer.getD_currentCountries().isEmpty()) {
+            l_currentArmySize = Math.max(3, Math.round((float) (p_eachPlayer.getD_currentCountries().size() / 3)));
         }
-        if(p_eachPlayer.getD_currentContinents() != null && !p_eachPlayer.getD_currentContinents().isEmpty()){
+        if (p_eachPlayer.getD_currentContinents() != null && !p_eachPlayer.getD_currentContinents().isEmpty()) {
             int l_totalContinentValue = 0;
-            for(Continent l_eachContinent : p_eachPlayer.getD_currentContinents()){
+            for (Continent l_eachContinent : p_eachPlayer.getD_currentContinents()) {
                 l_totalContinentValue += l_eachContinent.getD_continentValue();
             }
             l_currentArmySize += l_totalContinentValue;
@@ -98,7 +97,7 @@ public class PlayerController {
 
     public boolean isUnallocatedArmiesExist(CurrentState p_currentState) {
         int l_totalCount = 0;
-        for(Player l_eachPlayer : p_currentState.getD_players()){
+        for (Player l_eachPlayer : p_currentState.getD_players()) {
             l_totalCount += l_eachPlayer.getD_unallocatedArmies();
         }
         return l_totalCount != 0;
@@ -106,32 +105,44 @@ public class PlayerController {
 
     public void createDeployOrder(String p_command, Player p_player) {
         List<Orders> l_orders;
-        if(p_player.getD_orders() == null || p_player.getD_orders().isEmpty()){
+        if (p_player.getD_orders() == null || p_player.getD_orders().isEmpty()) {
             l_orders = new ArrayList<>();
-        }
-        else{
+        } else {
             l_orders = p_player.getD_orders();
         }
+
         String l_countryName = p_command.split(" ")[1];
-        String l_noOfArmiesToDeploy = p_command.split(" ")[2];
-        if(validateDeployArmiesOrder(p_player, l_noOfArmiesToDeploy)){
+        int l_noOfArmiesToDeploy = Integer.parseInt(p_command.split(" ")[2]);
+
+        if (!validateCountryBelongstoPlayer(p_player, l_countryName)) {
+            System.out.println("The country " + l_countryName + " does not belong to this player.");
+        }
+        else if (validateNoOfArmiesToDeploy(p_player, l_noOfArmiesToDeploy)) {
             System.out.println("Given deploy order can't be executed as armies in deploy order is more than players unallocated armies");
         }
-        else{
-            Orders l_order = new Orders(p_command.split(" ")[0], l_countryName, Integer.parseInt(l_noOfArmiesToDeploy));
+        else {
+            Orders l_order = new Orders(p_command.split(" ")[0], l_countryName, l_noOfArmiesToDeploy);
             l_orders.add(l_order);
 
             p_player.setD_orders(l_orders);
 
-            Integer l_unallocatedArmies = p_player.getD_unallocatedArmies() - Integer.parseInt(l_noOfArmiesToDeploy);
+            Integer l_unallocatedArmies = p_player.getD_unallocatedArmies() - l_noOfArmiesToDeploy;
             p_player.setD_unallocatedArmies(l_unallocatedArmies);
 
             System.out.println("Order has been added to queue for execution");
         }
     }
 
-    private boolean validateDeployArmiesOrder(Player p_player, String p_noOfDeployArmies){
-        if(p_player.getD_unallocatedArmies() < Integer.parseInt(p_noOfDeployArmies)){
+    private boolean validateCountryBelongstoPlayer(Player p_player, String p_countryName) {
+        for (Country l_eachCountry : p_player.getD_currentCountries()) {
+            if (l_eachCountry.getD_countryName().equals(p_countryName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean validateNoOfArmiesToDeploy(Player p_player, int p_noOfDeployArmies){
+        if(p_player.getD_unallocatedArmies() < p_noOfDeployArmies){
             return true;
         }
         return false;
