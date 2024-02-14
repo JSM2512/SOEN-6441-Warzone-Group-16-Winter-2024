15384084e248a07cd4d419f3e340faaa2a224059
Jaiwant <jaiwantmahun2000@gmail.com>
2024-02-14 17:31:22 -0500

    Added : Test Cases for Map class

--- src/test/java/Controller/MainGameEngineTest.java
+++ src/test/java/Controller/MainGameEngineTest.java
@@ -1,11 +1,10 @@
 package Controller;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-class MainGameEngineTest {
+public class MainGameEngineTest {
 
     @Test
-    void main() {
+    public void main() {
     }
 }
