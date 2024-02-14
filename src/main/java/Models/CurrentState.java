cb849e229e6e33db7f7c4a264cf3433b9d533195
pankaj0412 <spankajk.53@gmail.com>
2024-02-14 22:07:01 +0530

    Fix : Optimized functions in Current State Model

--- src/main/java/Models/CurrentState.java
+++ src/main/java/Models/CurrentState.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class CurrentState {
     List<Player> d_players;
-    List<Orders> d_pendingOrders;
     Map d_map;
 
     public List<Player> getD_players() {
@@ -14,14 +13,6 @@ public class CurrentState {
 
     public void setD_players(List<Player> p_players) {
         this.d_players = p_players;
-    }
-
-    public List<Orders> getD_pendingOrders() {
-        return d_pendingOrders;
-    }
-
-    public void setD_pendingOrders(List<Orders> p_pendingOrders) {
-        this.d_pendingOrders = p_pendingOrders;
     }
 
     public Map getD_map() {
